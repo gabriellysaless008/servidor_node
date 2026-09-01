8b8dbd6714f734b5a29b82816403bcda15ed6c19
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-09-01 09:33:55 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// Inmportar o módulo nativo 'http' do Node.js
+const http = require('http');
+
+// Definir o endereço (localhost - Minha Máquina Local) e a porta onde o servidor vai escutar
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Cria o servidor web
+const server = http.createServer( (req, res) => {
+
+    // Define o status HTTP como 200 (OK) e o tipo de conteúdo como texto plano em UTF-8
+});
+
+
+
