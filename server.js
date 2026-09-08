8e43a0af90063e88237d76c47affd7bad9882079
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-09-08 08:45:57 -0300

    adicionei o 404

--- server.js
+++ server.js
@@ -18,11 +18,16 @@ const server = http.createServer((req, res) => {
         return res.end('<h1>Lista de Alunos</h1>'); // O return impede a execução das linhas de baixo
     }
 
+    // Se nenhuma rota acima for satisfeita, cai no 404
+    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+ 
+
   // Define o status HTTP como 200 (OK) e o tipo de conteudo como texto plano em UTF-8
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  //res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
  
   // Envia a resposta para o navegador/cliente
-  res.end('<h1>Olá, alunos! O servidor Node.js está rodando com sucesso!\n<h1>');
+  res.end('<h1 style="color: red";>404 - rota não encontrada\n<h1>');
+  //res.end('<h1>Olá, alunos! O servidor Node.js está rodando com sucesso!\n<h1>');
 });
 
 // Faz o servidor comecar a escutar na porta definida
